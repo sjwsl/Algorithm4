7c75cc943c6c582d027482ee9337449bc156bdad
sjwsl <xts.sjwsl@gmail.com>
2019-10-14 23:32:00 +0800

    Update FastCollinearPoints.java

--- src/FastCollinearPoints.java
+++ src/FastCollinearPoints.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Objects;
 
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdDraw;
@@ -9,16 +10,21 @@ public class FastCollinearPoints {
     private LineSegment[] segments;
     private ArrayList<String> visited = new ArrayList<String>();
 
-    private ArrayList<LineSegment> collinear(Point[] points, Point center) {
+    private ArrayList<LineSegment> collinear(Point[] ppoints, int center) {
         ArrayList<LineSegment> segments = new ArrayList<LineSegment>();
-        Arrays.sort(points, center.slopeOrder());
+        Point[] points=new Point[ppoints.length-center-1];
+        for(int i=center+1;i<ppoints.length;i++){
+            points[i-center-1]=ppoints[i];
+        }
+
+        Arrays.sort(points, ppoints[center].slopeOrder());
         for (int i = 0; i < points.length; i++) {
-            double nowSlope = points[i].slopeTo(center);
+            double nowSlope = points[i].slopeTo(ppoints[center]);
             int last = i;
-            while (last + 1 < points.length && points[last + 1].slopeTo(center) == nowSlope) ++last;
+            while (last + 1 < points.length && points[last + 1].slopeTo(ppoints[center]) == nowSlope) ++last;
             if (last - i + 1 >= 3) {
                 ArrayList<Point> sameSlopePoints = new ArrayList<Point>();
-                sameSlopePoints.add(center);
+                sameSlopePoints.add(ppoints[center]);
                 for (int j = i; j <= last; j++) sameSlopePoints.add(points[j]);
                 Point[] sameSlopePointsArray = sameSlopePoints.toArray(new Point[sameSlopePoints.size()]);
                 Arrays.sort(sameSlopePointsArray);
@@ -38,18 +44,18 @@ public class FastCollinearPoints {
 
         if (points == null) throw new IllegalArgumentException();
 
-        for (int i = 0; i < points.length; i++) {
+        for (int i = 0; i + 3 < points.length; i++) {
             if (points[i] == null) throw new IllegalArgumentException();
         }
 
         for (int i = 0; i < points.length; i++)
             for (int j = i + 1; j < points.length; j++) {
-                if (points[i].toString() == points[j].toString()) throw new IllegalArgumentException();
+                if (points[i].toString().equals(points[j].toString())) throw new IllegalArgumentException();
             }
 
         ArrayList<LineSegment> segments = new ArrayList<LineSegment>();
         for (int i = 0; i < points.length; i++) {
-            segments.addAll(collinear(points, points[i]));
+            segments.addAll(collinear(points, i));
         }
 
 
@@ -59,16 +65,21 @@ public class FastCollinearPoints {
 
     // the number of line segments
     public int numberOfSegments() {
-        LineSegment[] copySegments=segments;
+        LineSegment[] copySegments=segments.clone();
         return copySegments.length;
     }
 
     // the line segments
     public LineSegment[] segments() {
-        LineSegment[] copySegments=segments;
+        LineSegment[] copySegments=segments.clone();
         return copySegments;
     }
     public static void main(String[] args) {
+        String a=new String("aa");
+        String b=new String("bb");
+        StdOut.println(a==b);
+
+
         // read the n points from a file
         int n = StdIn.readInt();
         Point[] points = new Point[n];
