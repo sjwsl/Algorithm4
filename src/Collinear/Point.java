a716b6d181307b906db31008b1738f1022dbcf9f
sjwsl <xts.sjwsl@gmail.com>
2019-10-16 13:48:54 +0800

    Update Point.java

--- src/Collinear/Point.java
+++ src/Collinear/Point.java
@@ -55,7 +55,7 @@ public class Point implements Comparable<Point> {
         if (this.x == that.x && this.y == that.y) return Double.NEGATIVE_INFINITY;
         if (this.x == that.x) return Double.POSITIVE_INFINITY;
         if (this.y == that.y) return +0.0;
-        return (double)(this.y - that.y) / (this.x - that.x);
+        return (double) (this.y - that.y) / (this.x - that.x);
     }
 
 
