87e46b78a52b697d8b10f490c12091ab74602fa2
sjwsl <xts.sjwsl@gmail.com>
2019-10-28 21:47:19 +0800

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,8 @@
 import edu.princeton.cs.algs4.StdOut;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Iterator;
 
 public class Board {
 
@@ -12,7 +16,7 @@ public class Board {
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles) {
-        n = tiles[0].length;
+        n = tiles.length;
         this.tiles = tiles;
         StringBuffer stringBuffer = new StringBuffer();
         stringBuffer.append(n);
@@ -20,8 +24,8 @@ public class Board {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (tiles[i][j] > 0) {
-                    hamming += tiles[i][j] == (i * n) + j + 1 ? 1 : 0;
-                    manhattan += tiles[i][j] > (i * n) + j ? tiles[i][j] - ((i * n) + j) : (i * n + j) - tiles[i][j];
+                    hamming += tiles[i][j] == (i * n) + j + 1 ? 0 : 1;
+                    manhattan += Math.abs((tiles[i][j] - 1) / n - i) + Math.abs((tiles[i][j] - 1) % n - j);
                 }
                 String tile = Integer.toString(tiles[i][j]);
                 while (tile.length() < 3) tile = " " + tile;
@@ -55,27 +59,33 @@ public class Board {
 
     // is this board the goal board?
     public boolean isGoal() {
-        return
+        return hamming == 0;
     }
 
     // does this board equal y?
     public boolean equals(Object y) {
-
+        if (y == null) return false;
+        if (!(y instanceof Board)) return false;
+        return this.toString() == y.toString();
     }
 
     // all neighboring boards
     public Iterable<Board> neighbors() {
-
+        return null;
     }
 
     // a board that is obtained by exchanging any pair of tiles
     public Board twin() {
-
+        return null;
     }
 
     // unit testing (not graded)
     public static void main(String[] args) {
-
+        int[][] a = {{3, 2, 1}, {6, 4, 5}, {7, 8, 0}};
+        Board board = new Board(a);
+        StdOut.println(board.toString());
+        StdOut.println(board.hamming());
+        StdOut.println(board.manhattan());
     }
 
 }
